53dfc1f998321078d3749b7c9f7676824dad1407
Ana Camacho <32857099+Hanie24@users.noreply.github.com>
2018-10-30 15:32:52 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,36 +8,24 @@ function card (num) {
   for (var i = 1; i <= newArr.length; i += 2){
     newArr[i] = parseInt(newArr[i]*2);
     couple.push(newArr[i]);
-    // console.log(couple)
+    console.log(couple)
 
   }
 
-  // for (var i =var i < array.lengvar i++) {
-  //   const element = arvar[i];
-    
-  // }
   var odd = [];
   //Accediendo a los elementos en posición impar y sumarlos    
   for (var k = 0; k < newArr.length; k += 2){
     newArr[k] = parseInt(newArr[k]);
     var oddSum =+ odd.push(newArr[k]);
-    console.log(oddSum)
+    console.log(odd)
     
   }
-  // console.log(odd)
+  
 //Suma de los elementos pares e impares 
 //   var totalAmount = ;
 
 }
 
 
-//sumar los pares e impares
-// var resultado = (couple + odd); 
-// if(sum%10===0){
-// 	alert ("Su tarjeta es valida")
-// }
-// else{
-// 	alert ("Su tarjeta no es valida"); 
-// }
-// 
+ 
 

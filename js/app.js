c4455ba39f93b063d646b7094c3aa21803394d28
Ana Camacho <sailor_121108@hotmial.com>
2018-10-30 09:44:12 -0600

    correcciones

--- js/app.js
+++ js/app.js
@@ -1,32 +1,43 @@
-//Creando variable tarjeta de credito.
-var card = [4,2,0,1,9,9,0,0,0,0,8,2,5,6,8,8];
-//Invirtiendo los valores.
-card.reverse();
+//tarjeta de credito.
+card("4201990000825688");
 
-function isValidCard (card) {
+function card (num) {
+  var newArr = num.split('').reverse();
+  var couple = [];
+//Accediendo a los elementos en posición par y multiplicarlos.
+  for (var i = 1; i <= newArr.length; i += 2){
+    newArr[i] = parseInt(newArr[i]*2);
+    couple.push(newArr[i]);
+    // console.log(couple)
 
-//Creando bucle for para recorrer los elementos del Array
-  for (var i = 0; i <= card.length; i += 2){
- //Accediendo a los valores en posicion par    
-    var couple = (card[i]*2);
-//for anidado para ingresar a los valores de couple    
-    for (var k = 0; k <= couple.length; k++){
-      console.log(couple[k]);
-      //separar los digitos y sumarlos 
-    }
   }
-//Acceder a los numeros en posicion impar  
-  for (var j = 1; j <= card.length; j += 2){
-  	var odd = (card[j]);
+
+  // for (var i =var i < array.lengvar i++) {
+  //   const element = arvar[i];
+    
+  // }
+  var odd = [];
+  //Accediendo a los elementos en posición impar y sumarlos    
+  for (var k = 0; k < newArr.length; k += 2){
+    newArr[k] = parseInt(newArr[k]);
+    var oddSum =+ odd.push(newArr[k]);
+    console.log(oddSum)
+    
   }
+  // console.log(odd)
+//Suma de los elementos pares e impares 
+//   var totalAmount = ;
+
+}
+
+
 //sumar los pares e impares
-var resultado = (couple + odd); 
-if(sum%10===0){
-	alert ("Su tarjeta es valida")
-}
-else{
-	alert ("Su tarjeta no es valida"); 
-}
-}
-}
-isValidCard(card);
+// var resultado = (couple + odd); 
+// if(sum%10===0){
+// 	alert ("Su tarjeta es valida")
+// }
+// else{
+// 	alert ("Su tarjeta no es valida"); 
+// }
+// 
+

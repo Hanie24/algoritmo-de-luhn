820e4df23b1203b3af3edcf8e8eb6611686715cd
Ana Camacho <32857099+Hanie24@users.noreply.github.com>
2018-11-15 11:33:22 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,22 +2,32 @@
 card("4201990000825688");
 
 function card (num) {
-  var newArr = num.split('').reverse();
-  var couple = [];
+  let newArray = num.split('').reverse();
+  let couple = [];
+  let saveHigher = [];
+  let saveLess = [];
 //Accediendo a los elementos en posición par y multiplicarlos.
-  for (var i = 1; i <= newArr.length; i += 2){
-    newArr[i] = parseInt(newArr[i]*2);
-    couple.push(newArr[i]);
-    console.log(couple)
+  for (let i = 1; i <= newArray.length; i += 2){
+    newArray[i] = parseInt(newArray[i]*2);
+    couple.push(newArray[i]);
+    couple.map(function(couple){
+      if (couple > 9) {
+        saveHigher.push(couple);
+      } else {
+        saveLess.push(couple);
+      }
+    });
+    // let divide = Array.from(saveHigher);
+    console.log(saveHigher);
 
   }
 
-  var odd = [];
+  let odd = [];
   //Accediendo a los elementos en posición impar y sumarlos    
-  for (var k = 0; k < newArr.length; k += 2){
-    newArr[k] = parseInt(newArr[k]);
-    var oddSum =+ odd.push(newArr[k]);
-    console.log(odd)
+  for (let k = 0; k < newArray.length; k += 2){
+    newArray[k] = parseInt(newArray[k]);
+    let oddSum =+ odd.push(newArray[k]);
+    //console.log(odd)
     
   }
   
